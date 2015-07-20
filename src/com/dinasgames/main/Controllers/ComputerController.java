1f8435563514bfacdd8fda3d5354ee3d02a34e79
Jack Penny <admin@unique-games.co.uk>
2015-07-20 17:19:18 +0100

    Added Controllers

---
+++ src/com/dinasgames/main/Controllers/ComputerController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dinasgames.main.Controllers;
+
+/**
+ *
+ * @author Jack
+ */
+public class ComputerController extends Controller {
+    
+    @Override
+    public void update() {
+        
+    }
+    
+}
