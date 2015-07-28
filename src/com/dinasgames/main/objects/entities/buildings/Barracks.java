2636d9c634969381efc37917e7cc3e59d926f424
V414 <georgekentnye@live.co.uk>
2015-07-28 18:04:56 +0100

    Fixed barracks and resources class

--- src/com/dinasgames/main/objects/entities/buildings/Barracks.java
+++ src/com/dinasgames/main/objects/entities/buildings/Barracks.java
@@ -2,11 +2,15 @@ package com.dinasgames.main.objects.entities.buildings;
 
 import com.dinasgames.lwjgl.util.Color;
 import com.dinasgames.lwjgl.util.RectangleShape;
+import com.dinasgames.lwjgl.util.Renderer;
 import com.dinasgames.main.math.Vector2f;
 import com.dinasgames.main.objects.GameObjectType;
+import com.dinasgames.main.objects.RenderEvents;
+import com.dinasgames.main.objects.entities.Entity;
+import com.dinasgames.main.players.Player;
 import com.dinasgames.main.scenes.Scene;
 
-public class Barracks extends Building{
+public class Barracks extends Building implements RenderEvents{
   
   RectangleShape mShapeBody;
   RectangleShape mTower1;
@@ -15,103 +19,140 @@ public class Barracks extends Building{
   RectangleShape mTower4;
   
   public Barracks(Scene scene){
+    super(scene);
+
+    mHealthMax      = 1000.f;
+    mHealth         = mHealthMax;
+    mWidth          = 50.f;
+    mHeight         = 30.f;
     
-      super(scene);
-      
-    mShapeBody = null;
-    mTower1 = null;
-    mTower2 = null;
-    mTower3 = null;
-    mTower4 = null;
-    mHealthMax = 1000.f;
-    mHealth = mHealthMax;
+    Barracks self = this;
     
-    addToScene();
-    
+    this.addListener(new Entity.Events(){
+
+      @Override
+      public void onHealthChange(float oldHealth, float newHealth) {
+
+      }
+
+      @Override
+      public void onMaxHealthChange(float oldMaxHealth, float newMaxHealth) {
+
+      }
+
+      @Override
+      public void onNewOwner(Player oldOwner, Player newOwner) {
+        if(newOwner != null) {
+          self.setBodyColor( newOwner.getColor() );
+        }else{
+          self.setBodyColor( Color.WHITE );
+        }
+      }
+
+      @Override
+      public void onDeath() {
+
+      }
+
+      @Override
+      public void onSizeChange(Vector2f oldSize, Vector2f newSize) {
+
+      }
+
+      @Override
+      public void onSelected() {
+
+      }
+
+      @Override
+      public void onDeselected() {
+
+      }
+    });
   }
   
-  @Override
-  public void onCreate() {
-    super.onCreate();
-        
-    setSize(50.f, 30.f);
-    
-    mShapeBody = new RectangleShape(mSize);
-        
-    mShapeBody.setFillColor(Color.BLACK);
-    mShapeBody.setOutlineColor(Color.BLACK);
-    mShapeBody.setOutlineThickness(2.f);
-    mShapeBody.setOriginCenter();
-    mShapeBody.setScene(mScene);
-    mShapeBody.render(mRenderer);
-    
-    mTower1 = new RectangleShape(mSize.x/6, mSize.x/6);
-    mTower1.setFillColor(Color.BLACK);
-    mTower1.setOutlineColor(Color.BLACK);
-    mTower1.setOutlineThickness(2.f);
-    mTower1.setOrigin(mSize.x/2-3, mSize.y/2-3);
-    mTower1.setScene(mScene);
-    mTower1.render(mRenderer);
-    
-    mTower2 = new RectangleShape(mSize.x/6, mSize.x/6);
-    mTower2.setFillColor(Color.BLACK);
-    mTower2.setOutlineColor(Color.BLACK);
-    mTower2.setOutlineThickness(2.f);
-    mTower2.setOrigin(-mSize.x/2+mSize.x/6+3, mSize.y/2-3);
-    mTower2.setScene(mScene);
-    mTower2.render(mRenderer);
-    
-    mTower3 = new RectangleShape(mSize.x/6, mSize.x/6);
-    mTower3.setFillColor(Color.BLACK);
-    mTower3.setOutlineColor(Color.BLACK);
-    mTower3.setOutlineThickness(2.f);
-    mTower3.setOrigin(mSize.x/2-3, -mSize.y/2+mSize.x/6+3);
-    mTower3.setScene(mScene);
-    mTower3.render(mRenderer);
-    
-    mTower4 = new RectangleShape(mSize.x/6, mSize.x/6);
-    mTower4.setFillColor(Color.BLACK);
-    mTower4.setOutlineColor(Color.BLACK);
-    mTower4.setOutlineThickness(2.f);
-    mTower4.setOrigin(-mSize.x/2+3+mSize.x/6, -mSize.y/2+mSize.x/6+3);
-    mTower4.setScene(mScene);
-    mTower4.render(mRenderer);
-  }
+  public void setBodyColor(Color color) {
+        //mShapeBody.setFillColor(color);
+    }
   
-  @Override
-  public void onNewOwner() {
-    mShapeBody.setFillColor(mOwner.getColor());
-    mTower1.setFillColor(mOwner.getColor());
-    mTower2.setFillColor(mOwner.getColor());
-    mTower3.setFillColor(mOwner.getColor());
-    mTower4.setFillColor(mOwner.getColor());
-  }
-    
     @Override
-    public void onTick(double time) {
-      super.onTick(time);
+    public void onRenderAdd(Renderer r) {
+        
+        super.onRenderAdd(r);
+        
+        mShapeBody = new RectangleShape(mWidth, mHeight);
+        
+        mShapeBody.setFillColor(mOwnerColor);
+        mShapeBody.setOutlineColor(Color.BLACK);
+        mShapeBody.setOutlineThickness(2.f);
+        mShapeBody.setOriginCenter();
+
+        mTower1 = new RectangleShape(mWidth/6, mWidth/6);
+        mTower1.setFillColor(mOwnerColor);
+        mTower1.setOutlineColor(Color.BLACK);
+        mTower1.setOutlineThickness(2.f);
+        mTower1.setOrigin(mWidth/2-3, mHeight/2-3);
+
+        mTower2 = new RectangleShape(mWidth/6, mWidth/6);
+        mTower2.setFillColor(mOwnerColor);
+        mTower2.setOutlineColor(Color.BLACK);
+        mTower2.setOutlineThickness(2.f);
+        mTower2.setOrigin(-mWidth/2+mWidth/6+3, mHeight/2-3);
+
+        mTower3 = new RectangleShape(mWidth/6, mWidth/6);
+        mTower3.setFillColor(mOwnerColor);
+        mTower3.setOutlineColor(Color.BLACK);
+        mTower3.setOutlineThickness(2.f);
+        mTower3.setOrigin(mWidth/2-3, -mHeight/2+mWidth/6+3);
+
+        mTower4 = new RectangleShape(mWidth/6, mWidth/6);
+        mTower4.setFillColor(mOwnerColor);
+        mTower4.setOutlineColor(Color.BLACK);
+        mTower4.setOutlineThickness(2.f);
+        mTower4.setOrigin(-mWidth/2+3+mWidth/6, -mHeight/2+mWidth/6+3);
+        
+        r.add(mShapeBody);
+        r.add(mTower1);
+        r.add(mTower2);
+        r.add(mTower3);
+        r.add(mTower4);
+        
     }
     
     @Override
-    public void onRender() {
-      super.onRender();
-
-      mShapeBody.setPosition(mPosition);
+    public void onRenderUpdate(Renderer r) {
+      super.onRenderUpdate(r);
+        
+      mShapeBody.setPosition(mX, mY);
       mShapeBody.setRotation(mRotation);
-      
-      mTower1.setPosition(mPosition);
+
+      mTower1.setPosition(mX, mY);
       mTower1.setRotation(mRotation);
       
-      mTower2.setPosition(mPosition);
+      mTower2.setPosition(mX, mY);
       mTower2.setRotation(mRotation);
       
-      mTower3.setPosition(mPosition);
+      mTower3.setPosition(mX, mY);
       mTower3.setRotation(mRotation);
       
-      mTower4.setPosition(mPosition);
+      mTower4.setPosition(mX, mY);
       mTower4.setRotation(mRotation);
+        
     }
-  
+    
+    @Override
+    public void onRenderRemove(Renderer r) {
+        
+        super.onRenderRemove(r);
+        
+        r.remove(mShapeBody);
+        r.remove(mTower1);
+        r.remove(mTower2);
+        r.remove(mTower3);
+        r.remove(mTower4);
+        
+    }
+    
   @Override
   public int getTypeID() {
       return super.getTypeID() | GameObjectType.Barracks.getID();
--- src/com/dinasgames/main/objects/resources/Resource.java
+++ src/com/dinasgames/main/objects/resources/Resource.java
@@ -1,34 +1,30 @@
 package com.dinasgames.main.objects.resources;
 
+import com.dinasgames.lwjgl.util.Renderer;
 import com.dinasgames.main.objects.GameObject;
 import com.dinasgames.main.objects.GameObjectType;
+import com.dinasgames.main.objects.LogicEvents;
+import com.dinasgames.main.objects.RenderEvents;
+import com.dinasgames.main.objects.SceneEvents;
 import com.dinasgames.main.scenes.Scene;
+import com.dinasgames.main.system.Time;
 
-public class Resource extends GameObject{
+public class Resource extends GameObject implements SceneEvents, RenderEvents, LogicEvents{
   
   protected String mResourceType;
   protected int mResourceQuantity;
   
   protected Resource(Scene scene){
-    mScene = scene;
-    mRenderer = mScene.getRenderer();
-  }
-  
-  @Override
-  public void onDestroy() {
-
+    super(scene);
+    
+    
   }
 
   @Override
-  public void onTick(double time) {
+  public void onTick(Time timePassed) {
 
   }
 
-  @Override
-  public void onRender() {
-
-  }
-  
   @Override
   public int getTypeID() {
     return GameObjectType.Resource.getID();
@@ -50,5 +46,20 @@ public class Resource extends GameObject{
   }
   
   //Setters
+
+  @Override
+  public void onSceneAdd(Scene scene) {}
+
+  @Override
+  public void onSceneRemove(Scene scene) {}
+
+  @Override
+  public void onRenderAdd(Renderer renderer) {}
+
+  @Override
+  public void onRenderRemove(Renderer renderer) {}
+
+  @Override
+  public void onRenderUpdate(Renderer renderer) {}
   
 }
