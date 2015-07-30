017155c6a777fd683518b13552daa48f0d53852b
Jack Penny <admin@unique-games.co.uk>
2015-07-30 13:33:41 +0100

    * Fix Duplicate ID

--- src/com/dinasgames/main/objects/GameObjectType.java
+++ src/com/dinasgames/main/objects/GameObjectType.java
@@ -37,10 +37,10 @@ public enum GameObjectType {
     Warehouse       (0x00400000),
     NewBuilding     (0x00800000),
     MediumTank      (0x01000000),
-    HeavyTank       (0x02000000);
-    SeaInfantry     (0x01000000),
-    LandInfantry    (0x02000000),
-    AirInfantry     (0x04000000);
+    HeavyTank       (0x02000000),
+    SeaInfantry     (0x04000000),
+    LandInfantry    (0x08000000),
+    AirInfantry     (0x10000000);
     
     // Constructor
     private final int mId;
